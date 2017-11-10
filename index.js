e7cca6c173953937531f3fe1ebb54df1eba233b1
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-10 19:46:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = "Agent Dale Cooper";
-const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
