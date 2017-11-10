640f076493edcb10e5255b5ecf2053c16c8d3dae
Nathaniel Stiers <nvstiers@gmail.com>
2017-11-10 20:08:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = "Agent Dale Cooper";
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const = excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
